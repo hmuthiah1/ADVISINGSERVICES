a3f3d8f40250d2656ad2e7d43591ee331a9ba38e
hmuthiah1 <hmuthiah1@buffs.wtamu.edu>
2023-10-08 17:43:49 -0500

    Assignment 4
    User Story

--- AdvisingServices/Advising_Services/degree_checklist/admin 2.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import College, Department, Degree, Course, CurriculumGuide, DegreeChecklist
-
-# Register your models here.
-myModels = [College, Department, Degree, Course, CurriculumGuide, DegreeChecklist]  # iterable list
-admin.site.register(myModels)
