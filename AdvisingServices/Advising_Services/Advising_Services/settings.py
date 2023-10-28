85806823d59da8542873203a153d60406228b3ee
hmuthiah1 <hmuthiah1@buffs.wtamu.edu>
2023-10-28 07:37:22 -0500

    Assignment Six - Django admin import export
    Create a Django admin customization that takes a file upload where that upload can be used to import data into the database

--- AdvisingServices/Advising_Services/Advising_Services/settings.py
+++ AdvisingServices/Advising_Services/Advising_Services/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "degree_checklist"
+    "degree_checklist",
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- AdvisingServices/Advising_Services/degree_checklist/admin.py
+++ AdvisingServices/Advising_Services/degree_checklist/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import College, Department, Degree, Course, CurriculumGuide, DegreeChecklist, DegreeChecklistPdf
-
+    
 # Register your models here.
 myModels = [College, Department, Degree, Course, CurriculumGuide, DegreeChecklist, DegreeChecklistPdf]  # iterable list
-admin.site.register(myModels)
+admin.site.register(myModels, ImportExportModelAdmin)
